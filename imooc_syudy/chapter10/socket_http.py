9ce2370fc62c9bfb5d1a1d5a7ac1f181894b5a64
yangdepp <965093975@qq.com>
2018-08-01 22:28:01 +0800

    socket

---
+++ imooc_syudy/chapter10/socket_http.py
@@ -0,0 +1,36 @@
+# requests -> urllib ->socket
+import socket
+from urllib.parse import urlparse
+
+
+def get_url(url):
+    # 通过socket请求html
+
+    # 首先解析url
+    url = urlparse(url)
+    host = url.netloc
+    path = url.path
+    if path == '':
+        path = '/'
+    # 建立socket连接
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((host, 80))
+    # http协议
+    client.send('GET {} HTTP/1.1\r\nHost:{}\r\nConnection:close\r\n\r\n'.format(path, host).encode('utf8'))
+
+    # 将所有数据读取完成
+    data = b""
+    while True:
+        d = client.recv(1024)
+        if d:
+            data += d
+        else:
+            break
+
+    data = data.decode("utf8")
+    print(data)
+    client.close()
+
+
+if __name__ == '__main__':
+    get_url('http://www.baidu.com')
